cc368376f7677bdd6c5700373868b709efe15a68
isnow4ever <gaoyongzhuo@yeah.net>
2018-02-01 00:02:00 +0800

    rqt_gui_plugin

---
+++ src/scara_gui/include/rqt_scara_gui/my_plugin.h
@@ -0,0 +1,53 @@
+/*
+  Copyright 2016 Lucas Walter
+*/
+#ifndef RQT_SCARA_GUI_MY_PLUGIN_H
+#define RQT_SCARA_GUI_MY_PLUGIN_H
+
+// ROS Plugin Includes
+#include <rqt_gui_cpp/plugin.h>
+#include <ros/ros.h>
+
+// Message Includes
+#include <std_msgs/String.h>
+#include <sensor_msgs/JointState.h>
+#include <tf/transform_listener.h>
+#include <tf/LinearMath/Matrix3x3.h>
+
+#include <rqt_gui_cpp/plugin.h>
+#include <scara_gui/ui_my_plugin.h>
+#include <QWidget>
+
+namespace rqt_scara_gui
+{
+
+class MyPlugin
+  : public rqt_gui_cpp::Plugin
+{
+  Q_OBJECT
+public:
+  MyPlugin();
+  virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+  virtual void shutdownPlugin();
+  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings,
+      qt_gui_cpp::Settings& instance_settings) const;
+  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
+      const qt_gui_cpp::Settings& instance_settings);
+
+  // Comment in to signal that the plugin has a way to configure it
+  // bool hasConfiguration() const;
+  // void triggerConfiguration();
+
+  void newDataCallback(const sensor_msgs::JointState& msg);
+
+private:
+  Ui::MyPluginWidget ui_;
+  QWidget* widget_;
+
+  ros::Subscriber joint_states_subscriber;
+  ros::Subscriber err_states_subscriber;
+  ros::Subscriber running_states_subscriber;
+  ros::Publisher alarm_clear_publisher;
+};
+}  // namespace rqt_scara_gui
+#endif  // RQT_SCARA_GUI_MY_PLUGIN_H
---
+++ src/scara_gui/src/rqt_scara_gui/my_plugin.cpp
@@ -0,0 +1,80 @@
+/*
+  Copyright 2016 Lucas Walter
+*/
+
+#include "rqt_scara_gui/my_plugin.h"
+#include <pluginlib/class_list_macros.h>
+#include <QStringList>
+#include "rqt_scara_gui/indicatorlamp.h"
+
+namespace rqt_scara_gui
+{
+
+// Callbacks
+void MyPlugin::newDataCallback(const sensor_msgs::JointState& msg)
+{
+  ui_.jointState_1->setText(QString::number(msg.position.at(0), 10, 2));
+  ui_.jointState_2->setText(QString::number(msg.position.at(1), 10, 2));
+  ui_.jointState_3->setText(QString::number(msg.position.at(2), 10, 2));
+  ui_.jointState_4->setText(QString::number(msg.position.at(3), 10, 2));
+}
+
+
+MyPlugin::MyPlugin()
+  : rqt_gui_cpp::Plugin()
+  , widget_(0)
+{
+  // Constructor is called first before initPlugin function, needless to say.
+
+  // give QObjects reasonable names
+  setObjectName("MyPlugin");
+}
+
+void MyPlugin::initPlugin(qt_gui_cpp::PluginContext& context)
+{
+  // access standalone command line arguments
+  QStringList argv = context.argv();
+  // create QWidget
+  widget_ = new QWidget();
+  // extend the widget with all attributes and children from UI file
+  ui_.setupUi(widget_);
+  // add widget to the user interface
+  context.addWidget(widget_);
+  ui_.robotStateLamp->setAlarm(false);
+  ui_.comStateLamp->setAlarm(false);
+  ui_.enableStateLamp->setAlarm(false);
+  ui_.errorStateLamp->setAlarm(true);
+
+  // Subscribe to new data
+  joint_states_subscriber = getNodeHandle().subscribe ("/joint_states", 1, &rqt_scara_gui::MyPlugin::newDataCallback, this);
+}
+
+void MyPlugin::shutdownPlugin()
+{
+  // unregister all publishers here
+}
+
+void MyPlugin::saveSettings(qt_gui_cpp::Settings& plugin_settings,
+    qt_gui_cpp::Settings& instance_settings) const
+{
+  // instance_settings.setValue(k, v)
+}
+
+void MyPlugin::restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
+    const qt_gui_cpp::Settings& instance_settings)
+{
+  // v = instance_settings.value(k)
+}
+
+/*bool hasConfiguration() const
+{
+  return true;
+}
+
+void triggerConfiguration()
+{
+  // Usually used to open a dialog to offer the user a set of configuration
+}*/
+
+}  // namespace rqt_example_cpp
+PLUGINLIB_DECLARE_CLASS(rqt_scara_gui, MyPlugin, rqt_scara_gui::MyPlugin, rqt_gui_cpp::Plugin)
